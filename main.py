47f5e7cd0e9c72348e1caa7e96bd195254d572ef
capitao-red-beard <jasper.v.d.valk@gmail.com>
2019-09-03 09:38:50 +0200

    added some basic code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
 from openpyxl import load_workbook
 
-BASE_URL = r'https://vefa.difi.no/smp/'
+BASE_URL = r'https://vefa.difi.no/smp'
 
 wb = load_workbook('sap_sheet.xlsx', data_only=True)
-sheet = wb.get_sheet_by_name('2) Input sheet')
+sheet = wb['2) Input sheet']
 tax = sheet['G3'].value
+identifier = '9944'
+
+url = f'{BASE_URL}/{identifier}/{tax}'
+print(url)
+
+r = requests.get(url)
+print(r.status_code)
