dc498ba225efb971d1012d299ee2eb2bda4d4486
capitao-red-beard <jasper.v.d.valk@gmail.com>
2019-09-05 08:45:48 +0200

    formatted code to pep-8

--- main.py
+++ main.py
@@ -3,32 +3,32 @@ from openpyxl import load_workbook
 from bs4 import BeautifulSoup
 
 
-BASE_URL = r'https://vefa.difi.no/smp'
+BASE_URL = r"https://vefa.difi.no/smp"
 
 # Load the sheet and load in values rather than formulas.
-wb = load_workbook('sap_sheet.xlsx', data_only=True)
-sheet = wb['2) Input sheet']
-tax = sheet['G3'].value
+wb = load_workbook("sap_sheet.xlsx", data_only=True)
+sheet = wb["2) Input sheet"]
+tax = sheet["G3"].value
 
 # TODO retrieve the identifier from the excel sheet.
-identifier = '9922'
+identifier = "9922"
 
-url = f'{BASE_URL}/{identifier}/{tax}'
+url = f"{BASE_URL}/{identifier}/{tax}"
 
 r = requests.get(url)
-if 'not registered in SML.' in r.text:
-    print('error')
+if "not registered in SML." in r.text:
+    print("error")
     exit()
 
-soup = BeautifulSoup(r.text, 'html.parser')
+soup = BeautifulSoup(r.text, "html.parser")
 
 # 'dd' finds all the output values required for VAT.
-output = soup.findAll('dd')
-results_list = [str(i).replace('<dd>', '').replace('</dd>', '')
+output = soup.findAll("dd")
+results_list = [str(i).replace("<dd>", "").replace("</dd>", "")
                 for i in output]
 print(results_list)
 
 # Find all of the document formats returned in the page.
-documents = soup.findAll('small', {'class': 'meta'})
+documents = soup.findAll("small", {"class": "meta"})
 
 # TODO retrieve ONLY the values from the html.
